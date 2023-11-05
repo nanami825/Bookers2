ebf4912ebf3f55b72759116a2064d9779dea75a3
nanami825 <happiness_773_0218@yahoo.co.jp>
2023-11-05 22:43:07 +0900

    Bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @user = current_user 
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id) 
+    else
+      @books = Book.all
+      @user = current_user 
+      render :index
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    @user = @book.user
+    unless @user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @book_new = Book.new
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book.id)
+    else 
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
